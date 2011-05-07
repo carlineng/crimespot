b7c89729abb7bc67f8fb1b4a95dfa0321e306435
Carlin Eng <carlineng@gmail.com>
2011-05-06 17:41:56 -0700

    Fixed syntax errors, added some help info

--- src/py/CrimeSpottingApi.py
+++ src/py/CrimeSpottingApi.py
@@ -10,12 +10,46 @@ rootSuffix = '.crimespotting.org/crime-data?'
 class CrimeSpottingApi:
 
     def __init__(self, city="sf"):
+        self.setCity(city)
+
+    def setCity(self, city):
         if city == "sf":
             self.root = sfPrefix + rootSuffix
-        elif city == "oak"
+        elif city == "oak":
             self.root = oakPrefix + rootSuffix
+        else:
+            print "Did not recognize city: " + str(city)
 
-    def getCsv(**kwargs):
+    def help(self, param=None):
+        if param is None:
+            print ("Possible arguments are: type, dstart, dend, dstart, dtend, bbox, count, offset.\n\nCall help(<arg>) for more information on a specific argument")
+
+        if param == "type":
+            print ("Crime types to get, in a comma separated list.\nValid options are: aggravated_assault, murder, robbery, simple_assault, arson, burglary, theft, vandalism, vehicle_theft, alcohol, disturbing_the_peace, narcotics, prostitution\nShort versions are also acceptable: AA, Mu, Ro, SA, Ar, Bu, Th, Va, VT, Al, DP, Na, Pr.\nNote that these are case-sensitive")
+
+        if param == "dstart":
+            pass
+
+        if param == "dend":
+            pass
+
+        if param == "dtstart":
+            pass
+
+        if param == "dtend":
+            pass
+
+        if param == "bbox":
+            pass
+        
+        if param == "count":
+            pass
+
+        if param == "offset":
+            pass
+
+
+    def getCsv(self, **kwargs):
         query = self.root
 
         params = ['format=csv']
