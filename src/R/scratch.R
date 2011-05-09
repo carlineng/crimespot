731e92f0dbea0a0913fa99071d699da783ef04d6
Carlin Eng <carlineng@gmail.com>
2011-05-09 14:13:23 -0700

    Simple ggplot2 graphics.  Scatter plot of crimes, faceted by crime type.

--- src/R/scratch.R
+++ src/R/scratch.R
@@ -2,6 +2,17 @@
 setwd('/Volumes/hobo/githobo/crimespot/src/R')
 source('CrimeSpottingApi.R')
 
+startYear = 2009
+startDate = as.Date(paste(startYear, "-01-01", sep=""))
+
+crime.2009 = data.frame();
+numDays = 365
+for(i in c(0:(numDays - 1))) {
+    print(paste("day", i))
+    dait = as.Date(i, origin = startDate)
+    crime.2009 = rbind(crime.2009, getCrimeOnDate(dait, count=10000))
+}
+
 startYear = 2010
 startDate = as.Date(paste(startYear, "-01-01", sep=""))
 
@@ -13,5 +24,23 @@ for(i in c(0:(numDays - 1))) {
     crime.2010 = rbind(crime.2010, getCrimeOnDate(dait, count=10000))
 }
 
-write.csv(crime.2010, file='../../data/crime_sf_2010.csv')
+startYear = 2011
+startDate = as.Date(paste(startYear, "-01-01", sep=""))
+
+crime.2011 = data.frame();
+numDays = 365
+for(i in c(0:(numDays - 1))) {
+    print(paste("day", i))
+    dait = as.Date(i, origin = startDate)
+    crime.2011 = rbind(crime.2011, getCrimeOnDate(dait, count=10000))
+}
+
+crime <- rbind(crime.2009, crime.2010, crime.2011)
+
+write.csv(crime, file='../../data/crime_sf.csv')
+
+p <- ggplot(crime, aes(x = Longitude, y = Latitude))
+p + facet_grid(. ~ Crime.Type) + geom_point(alpha = I(1/50))
+
+p + facet_wrap(~ Crime.Type, ncol=4) + geom_point(alpha = I(1/50))
 
